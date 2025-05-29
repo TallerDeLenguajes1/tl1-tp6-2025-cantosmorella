e8fae295da9854bae57f81c0edb3863db23ba904
Morella Cantos <cantosmorella@gmail.com>
2025-05-28 21:43:23 -0300

    Ej 2 en rama CalculadoraV1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.VisualBasic;
-
+/*
 Console.WriteLine("Hello, World!");
 int a;
 int b;
@@ -36,5 +36,53 @@ if (num > 0)
 } else {
     Console.WriteLine("No se puede invertir el numero por no ser mayor a cero");
 }
-
+*/
 //Ejercicio 2
+
+int operacion, num1, num2,resultado,control;
+string s1, s2, s, s3;
+
+void solicitarDatos()
+{
+    Console.WriteLine("Calculadora");
+    Console.WriteLine("Seleccione la operacion que deser realizar: ");
+    Console.WriteLine("1.Suma\n2.Resta\n3.Multiplicar\n4.Dividir");
+    s = Console.ReadLine();
+    bool conversion = int.TryParse(s, out operacion);
+
+    Console.WriteLine("Ingrese el primer numero");
+    s1 = Console.ReadLine();
+    bool conversion2 = int.TryParse(s1, out num1);
+
+    Console.WriteLine("Ingrese el primer numero");
+    s2 = Console.ReadLine();
+    bool conversion3 = int.TryParse(s2, out num2);
+}
+
+
+int calcularNumeros(int operacion, int num1,int num2)
+{
+    switch (operacion)
+    {
+        case 1:
+            return num1 + num2;
+        case 2:
+            return num1 - num2;
+        case 3:
+            return num1 * num2;
+        case 4:
+            return num1 / num2;
+        default: return 11111;
+    }
+}
+
+do
+{
+    solicitarDatos();
+    resultado = calcularNumeros(operacion, num1, num2);
+    Console.WriteLine("El resultado de su operacion es : " + resultado);
+    Console.WriteLine("desea realizar otra operacion? 1:SI 2:NO");
+    s3 = Console.ReadLine();
+    bool conversion4 = int.TryParse(s3, out control);
+} while (control==1);
+
